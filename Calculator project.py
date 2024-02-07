0039b82cb25ccb82f25b47ef37beef000b1a870d
Remusmonkey <airdogaron@aol.com>
2024-02-07 09:08:29 -0800

    testing addition

---
+++ Calculator project.py
@@ -0,0 +1,38 @@
+def addition(term_1, term_2):
+    return term_1 + term_2
+def subtraction(term_1, term_2):
+    return term_1 - term_2
+def multiply(term_1, term_2):
+    return term_1 * term_2
+def divide(term_1, term_2):
+    if term_2 == 0:
+        return "Cannot devide by 0.  Please use a non-zero number."
+    else:
+        return term_1 / term_2
+
+while True:
+    print('''Welcome to the calculator. Please select an operation.
+    1. Add
+    2. Subtract
+    3. Divide
+    4. Multiply
+    5. Exit''')
+    operation = int(input('Operation Number: '))
+    term_1 = float(input('First number:  '))
+    term_2 = float(input('Second number:  '))
+
+
+        if operation == 1:
+            print(term_1, "+", term_2, "=", addition(term_1, term_2))
+
+        elif operation == 2:
+            print(term_1, "-", term_2, "=", subtraction(term_1, term_2))
+
+        elif operation == 3:
+            print(term_1, "/", term_2, "=", divide(term_1, term_2))
+
+        elif operation == 4:
+            print(term_1, "*", term_2, "=", multiply(term_1, term_2))
+
+        else:
+            print("Exiting. Thank you for using THE Calculator!")
