7c2b843dd6716369d98f74f75bed3d31e3d250d7
Remusmonkey <airdogaron@aol.com>
2024-02-22 08:37:03 -0800

    testing addition

--- Calculator project.py
+++ Calculator project.py
@@ -6,7 +6,7 @@ def multiply(term_1, term_2):
     return term_1 * term_2
 def divide(term_1, term_2):
     if term_2 == 0:
-        return "Cannot devide by 0.  Please use a non-zero number."
+        return "Cannot divide by 0.  Please use a non-zero number."
     else:
         return term_1 / term_2
 
@@ -18,21 +18,26 @@ while True:
     4. Multiply
     5. Exit''')
     operation = int(input('Operation Number: '))
+
+
+    if operation == 5:
+        print("Exiting Calculator")
+        break
+
     term_1 = float(input('First number:  '))
     term_2 = float(input('Second number:  '))
 
+    if operation == 1:
+        print(term_1, "+", term_2, "=", addition(term_1, term_2))
 
-        if operation == 1:
-            print(term_1, "+", term_2, "=", addition(term_1, term_2))
+    elif operation == 2:
+        print(term_1, "-", term_2, "=", subtraction(term_1, term_2))
 
-        elif operation == 2:
-            print(term_1, "-", term_2, "=", subtraction(term_1, term_2))
+    elif operation == 3:
+        print(term_1, "/", term_2, "=", divide(term_1, term_2))
 
-        elif operation == 3:
-            print(term_1, "/", term_2, "=", divide(term_1, term_2))
+    elif operation == 4:
+        print(term_1, "*", term_2, "=", multiply(term_1, term_2))
 
-        elif operation == 4:
-            print(term_1, "*", term_2, "=", multiply(term_1, term_2))
-
-        else:
-            print("Exiting. Thank you for using THE Calculator!")
+    else:
+        print("Please choose a valid operation.")
