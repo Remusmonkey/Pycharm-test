1a779b811738264cb504088895f027278ff319b9
Remusmonkey <airdogaron@aol.com>
2024-03-01 14:41:59 -0800

    testing addition

--- Udemy blockchain project.py
+++ Udemy blockchain project.py
@@ -1,9 +1,16 @@
-blockchain = [1]
+blockchain = [[1]]
 
-def add_value():
-    blockchain.append([blockchain[0], 5.3])
-    print(blockchain)
 
-add_value()
-add_value()
-add_value()
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
+def add_value(transaction_amount):
+    blockchain.append([get_last_blockchain_value(), transaction_amount])
+
+
+add_value(2)
+add_value(.9)
+add_value(10.89)
+
+print(blockchain)
